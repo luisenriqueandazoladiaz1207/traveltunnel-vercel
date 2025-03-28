34375c1316c3dbd119ee6518fb7c102e53c51c5d
luisenriqueandazoladiaz1207 <jtvsuperoncejtv@gmail.com>
2025-03-27 23:14:07 -0600

    feat(provides): added new code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,12 @@
 <?php
 
+
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,11 +18,15 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
+ 
